21ad1ea17980e4fa80a6ea94680fce98c4412bd2
hellozhu <120171080607@buaa.edu.cn>
2019-12-13 17:42:35 +0800

    1.完成了向前、向后拓展的函数

--- src/main/java/demo/GraphOperator/Operator.java
+++ src/main/java/demo/GraphOperator/Operator.java
@@ -3,11 +3,15 @@ package demo.GraphOperator;
 import demo.Graph.Graph;
 
 public abstract class Operator {
-    public static Graph getResult(Graph graph){
-        return  null;
+    public static Graph getResult(Graph graph) {
+        return null;
     }
 
-    public static Graph getResult(Graph R,Graph S){
+    public static Graph getResult(Graph R, Graph S) {
+        return null;
+    }
+
+    public static Graph getResult(Graph R, Graph S, boolean flag) {
         return null;
     }
 }
--- src/main/java/demo/service/EdgeInsert.java
+++ src/main/java/demo/service/EdgeInsert.java
@@ -20,9 +20,10 @@ public class EdgeInsert {
 
         // Queue<NormalGraph> Q1 = new LinkedList<NormalGraph>();                                           //创建一个空队列Q
         SDGraph SD_r = new SDGraph();                                                                    //空的SDr
-        NormalGraph derta_P_r = (NormalGraph) MulOperator.getResult(derta_E_s, Eu);
+        // 论文中derta_P_r的运算与程序中的MulOperator完全是一个意思
+        NormalGraph derta_P_r = (NormalGraph) ForwardOperator.getResult(derta_E_s, Eu,true);
 
-        //当derta_SD_r不为空的时候循环
+        //当derta_SD_r不为空的时候循环，虽然这里和论文中有些不同，但是不影响最后的结果
         SDGraph derta_SD_r = (SDGraph) PruneOperator.getResult(MinOperator.getResult(derta_P_r), SD_star);
         while (!(derta_SD_r.isGraphNull())) {
             derta_SD_r = (SDGraph) PruneOperator.getResult(MinOperator.getResult(derta_P_r), SD_star);
@@ -35,11 +36,12 @@ public class EdgeInsert {
             SD_r = (SDGraph) AddOperator.getResult(derta_SD_r, SD_r);
 
             //12:   省略原文第12行
+            derta_P_r = (NormalGraph) ForwardOperator.getResult(derta_E_s, Eu,false);
         }
 
         // Queue<NormalGraph> Q2 = new LinkedList<NormalGraph>();
         SDGraph SD_l = new SDGraph();                                                                    //空的SDl
-        NormalGraph derta_P_l = (NormalGraph) MulOperator.getResult(Eu, derta_E_s);
+        NormalGraph derta_P_l = (NormalGraph) BackWardOperator.getResult(Eu, derta_E_s,true);
 
         SDGraph derta_SD_l = (SDGraph) PruneOperator.getResult(MinOperator.getResult(derta_P_l), SD_star);
         while (!(derta_SD_l.isGraphNull())) {
@@ -54,13 +56,15 @@ public class EdgeInsert {
             SD_l = (SDGraph) AddOperator.getResult(derta_SD_l, SD_l);
 
             //23:    省略原文第23行
+            derta_P_l = (NormalGraph) BackWardOperator.getResult(Eu,derta_SD_l,false);
         }
-
+//======================================================================================================================
+        // 下面一行的代码需要修改
+        // 需要单写一个类来处理。
         NormalGraph derta_P_i = (NormalGraph) MulOperator.getResult(SD_l, SD_r);
         SDGraph derta_SD_i = (SDGraph) PruneOperator.getResult(MinOperator.getResult(derta_P_i), SD_star);
 
         SD_star = (SDGraph) AddOperator.getResult(derta_SD_i, SD_star);
         return SD_star;
-
     }
 }
